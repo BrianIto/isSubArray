9650768119a72ca0a0e9d6ea8b9db3672863f3bf
Brian Ito <brian.moura@indt.org.br>
2023-03-11 17:51:21 -0400

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+	* @description Verifies if an array is a subarray of another (IT SEE IF THE ORDER IS RIGHT);
+	* @param {Array} parent array to be the searched; 
+	* @param {Array} a subarray that matches;
+*/
+const isSub = (array, subarray) => {
+	let auxIndex = 0;
+	let controller = [...subarray].fill(false);
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === subarray[0]) { auxIndex = i }
+		let subArrayIndex = i - auxIndex;
+		if (subArrayIndex < subarray.length) {
+			if (subarray[subArrayIndex] === array[i]) 	{ controller[subArrayIndex] = true }
+			if (subArrayIndex === subarray.length - 1) 	{ return !(controller.includes(false)) }
+		}
+	}
+	return false;
+}
+
+module.exports = isSub;
---
+++ test.spec.js
@@ -0,0 +1,11 @@
+const isSub = require("./index.js");
+
+test("Check if is subset successfully", () => {
+	expect(isSub([1,2,3], [1,2])).toBeTruthy()
+	expect(isSub([1,2,3], [1,3])).toBeFalsy()
+	expect(isSub([1,2,3], [2,3])).toBeTruthy()
+	expect(isSub(["oi", "sou", "Brian"], ["sou", "Brian"])).toBeTruthy()
+	expect(isSub(["Administrador", "de", "flores"], ["Administrador", "de", "materiais"])).toBeFalsy()
+	expect(isSub(["Administrador"], ["Administrador", "de", "materiais"])).toBeFalsy()
+});
+
